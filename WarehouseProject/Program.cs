5525e3e7851a13fb3b122396b2b124245c585caf
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-14 15:06:08 +0100

    incomplete

--- WarehouseProject/Program.cs
+++ WarehouseProject/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Item banana = new Item("banana", 1.50);
+            Item apple = new Item("apple", 1.00);
+            
             Warehouse warehouse = new Warehouse();
             Basket bobsBasket = new Basket();
             Console.WriteLine("Welcome to our warehouse!");
+            Warehouse.WarehouseContents.Add(banana.Name, 1);
         }
     }
 }
--- WarehouseProject/Warehouse.cs
+++ WarehouseProject/Warehouse.cs
@@ -2,10 +2,7 @@ namespace Warehouse
 {
     public class Warehouse
     {
-        Item banana = new Item("banana", 1.50);
-        Item apple = new Item("apple", 1.00);
-        Dictionary<string, int> WarehouseContents = new Dictionary<string, int>();
         
-
+        public Dictionary<string, int> WarehouseContents { get; set; }
     }
 }
